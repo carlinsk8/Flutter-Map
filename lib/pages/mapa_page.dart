ffe88530d3309e216fe492dd203f4431cd327291
cespana <carlin.08.12@gmail.com>
2020-10-19 13:25:29 -0500

    Trazado de ruta dentro del mapa

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -31,7 +31,11 @@ class _MapaPageState extends State<MapaPage> {
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        children: [BtnUbicacion()],
+        children: [
+          BtnUbicacion(),
+          BtnSeguirUbicacion(),
+          BtnMiRuta(),
+        ],
       ),
     );
   }
@@ -40,6 +44,8 @@ class _MapaPageState extends State<MapaPage> {
     if (!state.existeUbicacion) return Center(child: Text('Ubicando...'));
 
     final mapaBloc = BlocProvider.of<MapaBloc>(context);
+
+    mapaBloc.add(OnNuevaUbicacion(state.ubicacion));
 
     final camaraPosition =
         new CameraPosition(target: state.ubicacion, zoom: 15);
@@ -50,6 +56,10 @@ class _MapaPageState extends State<MapaPage> {
       myLocationButtonEnabled: false,
       zoomControlsEnabled: false,
       onMapCreated: mapaBloc.initMapa,
+      polylines: mapaBloc.state.polylines.values.toSet(),
+      onCameraMove: (cameraPosition) {
+        mapaBloc.add(OnMovioMapa(camaraPosition.target));
+      },
     );
   }
 }
